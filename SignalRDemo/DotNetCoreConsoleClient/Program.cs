460825c39eb05bfb9d22878fcecbce9fb20cf757
Nachiappan <nachiappan.nk.ext@sivantos.com>
2019-09-01 00:57:00 +0530

    Dot net Core Client

---
+++ SignalRDemo/DotNetCoreConsoleClient/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.AspNet.SignalR.Client;
+
+namespace DotNetCoreConsoleClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HubConnection hub = new HubConnection("http://localhost:64277");
+            IHubProxy proxy = hub.CreateHubProxy("TimeQuery");
+
+            proxy.On("addMessage", message =>
+            {
+                Console.WriteLine(message);
+            });
+
+            hub.Start().Wait();
+            proxy.Invoke("StartTimer");
+
+            string line = null;
+            while ((line = Console.ReadLine()) != null)
+            {
+            }
+        }
+    }
+}
